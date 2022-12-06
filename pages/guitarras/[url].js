53fd28932dc33ee1d857e91d22ef09f91268dd40
Juan M <juanm0677@gmail.com>
2022-12-06 18:01:02 -0400

    routing dinamico con getserversideprops

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,7 +1,18 @@
+export default function Producto({guitarras}) {
+    console.log(guitarras[0].attributes.nombre)
+  return <div>Producto</div>;
+}
 
-export default function Producto() {
-    
-  return (
-    <div>Producto</div>
-  )
+export async function getServerSideProps({ query: { url } }) {
+  const respuesta = await fetch(
+    `${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`
+  );
+  const { data: guitarras } = await respuesta.json();
+ 
+
+  return {
+    props: {
+        guitarras
+    },
+  };
 }
