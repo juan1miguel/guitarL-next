2ba61129b6be86f424a10ae0f256978d09035736
Juan M <juanm0677@gmail.com>
2022-12-13 22:03:48 -0400

    mostrando la informacion de la guitarra

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,18 +1,60 @@
-export default function Producto({guitarras}) {
-    console.log(guitarras[0].attributes.nombre)
-  return <div>Producto</div>;
+import Image from "next/image";
+import styles from "../../styles/guitarras.module.css";
+import Layout from "../../components/layout";
+export default function Producto({ guitarra }) {
+  const { nombre, descripcion, precio, imagen } = guitarra[0].attributes;
+  return (
+    <Layout title={`Guitarra ${nombre}`}>
+      <div className={styles.guitarra}>
+        <Image
+          src={imagen.data.attributes.url}
+          alt={`Imagen guitarra ${nombre}`}
+          width={600}
+          height={400}
+        />
+        <div className={styles.contenido}>
+          <h3>{nombre} </h3>
+          <p className={styles.descripcion}> {descripcion} </p>
+          <p className={styles.precio}>${precio} </p>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-export async function getServerSideProps({ query: { url } }) {
+// export async function getServerSideProps({ query: { url } }) {
+//   const respuesta = await fetch(
+//     `${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`
+//   );
+//   const { data: guitarras } = await respuesta.json();
+//   return {
+//     props: {
+//       guitarras,
+//     },
+//   };
+// }
+export async function getStaticPaths() {
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras`);
+  const { data } = await respuesta.json();
+
+  const paths = data.map((guitarra) => ({
+    params: {
+      url: `${guitarra.attributes.url}`,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+export async function getStaticProps({ params: { url } }) {
   const respuesta = await fetch(
     `${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`
   );
-  const { data: guitarras } = await respuesta.json();
- 
-
+  const { data: guitarra } = await respuesta.json();
   return {
     props: {
-        guitarras
+      guitarra,
     },
   };
 }
