969b544898b3dff54f2c77ef20a31a489453ccfe
Juan M <juanm0677@gmail.com>
2022-12-16 09:49:14 -0400

    consultando los post

--- pages/blog.js
+++ pages/blog.js
@@ -1,13 +1,34 @@
 import Layout from "../components/layout";
-export default function Blog() {
+import Post from "../components/post";
+export default function Blog({ posts }) {
   return (
     <div>
       <Layout
-        title={'Blog'}
-        description='Blog de musica venta de guitarras,consejo guitarLA'
+        title={"Blog"}
+        description="Blog de musica venta de guitarras,consejo guitarLA"
       >
-        <h2>Blog</h2>
+        <main className="contenedor">
+          <h2 className="heading">Blog</h2>
+          <div>
+            {posts.map((post) =>
+             (
+
+               <Post key={post.id} post={post.attributes} />
+             )
+              
+            )}
+          </div>
+        </main>
       </Layout>
     </div>
   );
+}
+export async function getStaticProps() {
+  const respuesta = await fetch(`${process.env.API_URL}/posts?populate=imagen`);
+  const { data: posts } = await respuesta.json();
+  return {
+    props: {
+      posts,
+    },
+  };
 }
